94bb5a5bd3313de945307352e1506d0329c28bd2
Becky Robran <rerobran@gmail.com>
2019-10-09 10:38:07 -0600

    Cleaned up spacing

--- test/room_test.rb
+++ test/room_test.rb
@@ -8,17 +8,21 @@ class RoomTest < Minitest::Test
   def setup
     @room = Room.new(:bedroom, 10, 13)
   end
+
   def test_it_exists
     assert_instance_of Room, @room
   end
+
   def test_it_initializes
     assert_equal :bedroom, @room.category
     assert_equal 10, @room.length
     assert_equal 13, @room.width
   end
+
   def test_it_has_an_area
     assert_equal 130, @room.area
     @room = Room.new(:bedroom, 10, 0)
     assert_equal "Please use a number greater than 0", @room.area
   end
-end 
+
+end
